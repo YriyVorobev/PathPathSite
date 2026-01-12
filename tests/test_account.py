eb26eef97bc44912c13d8c7993a4cfcd5ea4caea
Юрий Воробьёв <yriyvorobev@MacBook-Pro-Urij.local>
2026-01-12 22:40:05 +0300

    Добавлена сторя детейлев

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,8 +1,7 @@
 import time
-from time import sleep
-
 import allure
 from base.base_test import BaseTest
+from time import sleep
 
 @allure.epic("Test Changed")
 @allure.feature("Login in")
@@ -23,21 +22,25 @@ class TestAccount(BaseTest):
         self.my_info_page.is_opened()
 
     @allure.story("test fill out my info page")
-    def test_my_info_fill_out(self):
-        self.my_info_page.personal_details.change_first_name("Доулинго")
-        self.my_info_page.personal_details.changed_middle_name("Доулингович")
-        self.my_info_page.personal_details.last_name_changed("Даркнетович")
-        self.my_info_page.personal_details.changed_employee_id("12345")
-        self.my_info_page.personal_details.changed_other_id("12345")
-        self.my_info_page.personal_details.drivers_licence_numbers("12330")
-        self.my_info_page.personal_details.licence_expire_date("12-24-2025")
-        self.my_info_page.personal_details.date_of_birth("10-12-1997")
+    def test_my_info_fill_out(self,faker_ru):
+        self.my_info_page.personal_details.change_first_name(faker_ru.first_name())
+        self.my_info_page.personal_details.changed_middle_name(faker_ru.middle_name())
+        self.my_info_page.personal_details.last_name_changed(faker_ru.last_name())
+        self.my_info_page.personal_details.changed_employee_id(faker_ru.iana_id())
+        self.my_info_page.personal_details.changed_other_id(faker_ru.iana_id())
+        self.my_info_page.personal_details.drivers_licence_numbers(faker_ru.iana_id())
+        self.my_info_page.personal_details.licence_expire_date("2023-23-10")
+        self.my_info_page.personal_details.date_of_birth("2020-12-12")
         self.my_info_page.personal_details.gender_male()
         self.my_info_page.personal_details.save_changed()
-        self.my_info_page.personal_details.test_field("323434")
+        self.my_info_page.personal_details.test_field(faker_ru.postcode())
         self.my_info_page.personal_details.browser_load()
         self.my_info_page.personal_details.click_button_save()
+
+    @allure.story("test contact detail")
+    def test_contact_detail(self,faker_ru):
         self.my_info_page.go_to_contact_detail()
+        self.my_info_page.contact_details.address_street1(faker_ru.street_name())
 
 
 
